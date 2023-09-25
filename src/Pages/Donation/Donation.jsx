020487115dfdabaf92473102250c085f6d789577
LordShiva <litonnath4888@gmail.com>
2023-09-25 21:35:25 +0600

    Added pei Chart and make this dynamic

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,25 +4,61 @@ import { useEffect, useState } from "react";
 import DonationDetails from "../../Components/Donation/DonationDetails";
 
 const Donation = () => {
-  const donates= useLoaderData()
+  const donates = useLoaderData();
   const [getDonates, setDonates] = useState([]);
 
-  useEffect(()=>{
-    const getId = getDonate()
-    const filterDonates = donates.filter(donate => getId.includes(donate.id)) 
-    setDonates(filterDonates)
+  const [dataLength, setLength] = useState(4);
 
-  },[donates])
+  const [donatePei, setDonatePei] = useState();
 
-  // const getId = getDonate()
-  // console.log(getId)
+  useEffect(() => {
+    const getId = getDonate();
+    const filterDonates = donates.filter((donate) => getId.includes(donate.id));
+    setDonates(filterDonates);
+    setDonatePei(filterDonates.length)
+  }, [donates]);
+
+
+
+
+  const handleBtn = () => {
+    const allData = getDonates.length;
+    setLength(allData);
+  };
+  const handleBtnLess = (num) => {
+    const allData = num;
+    setLength(allData);
+  };
+
   return (
-    <div className="container my-20 mx-auto grid lg:grid-cols-2 gap-5">
-      {
-        getDonates.map(donation => <DonationDetails key={donation.id} donation={donation}></DonationDetails>)
-      }
+    <div className="m-10">
+      <div className="container mb-10 mx-auto grid lg:grid-cols-2 gap-5">
+        {getDonates.slice(0, dataLength).map((donation) => (
+          <DonationDetails
+            key={donation.id}
+            donation={donation}
+          ></DonationDetails>
+        ))}
+      </div>
+      <div className={dataLength === getDonates.length ? "hidden" : ""}>
+        <div className="flex justify-center">
+          <button onClick={handleBtn} className="btn bg-[#009444] text-white">
+            See More
+          </button>
+        </div>
+      </div>
+      <div className={getDonates.length > dataLength ? 'hidden': ''}>
+        <div className="flex justify-center">
+          <button
+            onClick={() => handleBtnLess(4)}
+            className="btn bg-[#009444] text-white"
+          >
+            See Less
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Donation;
+export default Donation ;
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,13 +1,25 @@
-import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Bar, Cell, ResponsiveContainer } from "recharts";
+import { getDonate } from "../../Utility/LocalStroage";
+import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Statistics = () => {
+  const [updateData, setUpdateData] = useState();
+
+  const givenDonate = getDonate();
+
+  const totalDonate = useLoaderData();
+
+  useEffect(() => {
+    const dataAfterDonate = totalDonate.length - givenDonate.length;
+    setUpdateData(dataAfterDonate);
+  }, [givenDonate, totalDonate]);
+
   const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
+    { name: "Group A", value: updateData },
+    { name: "Group B", value: totalDonate.length },
   ];
-
-  const COLORS = ["#00C49F", "#FF444A"];
-
+  const COLORS = ["#FF444A", "#00C49F"];
   const RADIAN = Math.PI / 180;
 
   const renderCustomizedLabel = ({
@@ -37,30 +49,36 @@ const Statistics = () => {
 
   return (
     <div>
-      <ResponsiveContainer width="100%" height={600}>
-        <PieChart>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-            label={renderCustomizedLabel}
-          >
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-      <div className="flex justify-center gap-10">
-        <p>Your Donation</p>
-        <p>Total Donation</p>
+      <div>
+        <ResponsiveContainer width="100%" height={300}>
+          <PieChart>
+            <Pie
+              data={data}
+              labelLine={false}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+              label={renderCustomizedLabel}
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="flex gap-10 md:flex-row flex-col justify-center">
+        <div className="flex items-center gap-3 justify-center">
+          <h2>Your Donation </h2>
+          <div className="w-10 h-2 bg-[#00C49F]"></div>
+        </div>
+        <div className="flex items-center gap-3 justify-center">
+          <h2>Total Donation </h2>
+          <div className="w-10 h-2 bg-[#FF444A]"></div>
+        </div>
       </div>
     </div>
   );
